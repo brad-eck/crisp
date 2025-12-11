f82a59fc0c9b9becc4d7b63ceb2daf4f227f912c
brad-eck <bradyeckma@gmail.com>
2025-12-10 21:16:08 -0700

    defined starting values for task object

--- task.go
+++ task.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Task struct {
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
+	Status   string `json:"status"` // "Todo", "In Progress", "Done"
+	Complete bool   `json:"complete"`
+}
